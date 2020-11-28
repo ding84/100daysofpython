6fba5f3576f431a026ef7c51b7f617d6f58baf1b
ding84 <webentwickler@online.de>
2020-11-28 14:24:02 +0100

    day 14 - added steps to py file

--- day14.py
+++ day14.py
@@ -57,3 +57,30 @@ def game():
       print(f"Sorry, that's wrong. Final score: {score}")
 
 game()
+
+
+#
+# breakdown of steps needed
+#
+
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
