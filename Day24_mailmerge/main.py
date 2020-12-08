f317089299a28b052b0cadbf10c7051a56a69571
ding84 <webentwickler@online.de>
2020-12-08 13:03:27 +0100

    Day 24 - Working with files

---
+++ Day24_mailmerge/main.py
@@ -0,0 +1,14 @@
+PLACEHOLDER = "[name]"
+
+invited_names = []
+with open("./Input/Names/invited_names.txt", "r") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.docx") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip("\n")
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        # print(new_letter)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.docx", "w") as completed_letter:
+            completed_letter.write(new_letter)
